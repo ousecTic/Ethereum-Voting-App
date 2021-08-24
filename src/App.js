ff104885785e6d8c85cc27dcb4814d0f9e34cfdb
Henry Ly <henryly213@gmail.com>
2021-08-24 17:24:15 -0500

    Add Score Board for red and blue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import './App.css';
+import OnBoardingButton from "./OnboardingButton"
+import Voting from "./artifacts/contracts/Voting.sol/Voting.json"
+
+const votingAddress = "0x0165878A594ca255338adfa4d48449f69242Eb8F"
 
 function App() {
+
+  let [account, setAccount] = useState("Guest")
+  let [blueScore, setBlueScore] = useState(0)
+  let [redScore, setRedScore] = useState(0)
+
+  async function fetchScore() {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(votingAddress, Voting.abi, provider);
+      const score = await contract.showScore();
+      console.log("Score: ", score);
+      setRedScore(score[0]);
+      setBlueScore(score[1])
+    }
+  }
+
+  useEffect(() => {
+    fetchScore();
+  }, [])
+
+  async function vote(color) {
+    try {
+      if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(votingAddress, Voting.abi, signer)
+      const transaction = await contract.castVote(color)
+      await transaction.wait()
+    }
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>BlockBank</h1>
+        <h3>{`Welcome ${account}`}</h3>
+        <OnBoardingButton setAccount={setAccount} />
+        <h4>{`Blue : ${blueScore}`}</h4>
+        <h4>{`Red : ${redScore}`}</h4>
+        <button onClick={() => vote("red")}>Vote Red</button>
+        <button onClick={() => vote("blue")}>Vote Blue</button>
       </header>
     </div>
   );
